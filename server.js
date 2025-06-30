81a0391f893e60982713db3c28ca62cfbbb133c3
Ahmet Sergen Boncuk <ahmet.sergen7@gmail.com>
2025-06-30 22:48:47 +0300

    cors configuretaion added to server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const sequelize = require('./config/database');
 const port = process.env.PORT;
+const cors = require('cors');
 
 // Routes
 const authRoutes = require("./routes/auth");
